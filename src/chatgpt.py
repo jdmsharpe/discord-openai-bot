a955e1feba4be904782585af0698306116b64751
John Magnus-Sharpe <jdmsharpe@gmail.com>
2024-05-07 21:12:32 -0700

    Overhaul to fix bugs and standardize against openai API

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from discord.commands import slash_command, option, OptionChoice
 from discord import ApplicationContext, Colour, Embed, File
 from pathlib import Path
+from typing import Optional
 
 from config.auth import GUILD_IDS, OPENAI_API_KEY
 
@@ -39,18 +40,18 @@ class ChatGPT(commands.Cog):
 
     @slash_command(
         name="chat",
-        description="Generate text in response to prompt.",
+        description="Creates a model response for the given chat conversation.",
         guild_ids=GUILD_IDS,
     )
     @option("prompt", description="Prompt", required=True)
     @option(
         "personality",
-        description="A description of what role you want the model to emulate",
+        description="What role you want the model to emulate. (default: You are a helpful assistant.)",
         required=False,
     )
     @option(
         "model",
-        description="Choose from the following GPT models (default: GPT-4 Turbo for cheaper cost)",
+        description="Choose from the following GPT models (default: gpt-4-turbo)",
         required=False,
         choices=[
             OptionChoice(name="GPT-3.5 Turbo", value="gpt-3.5-turbo-0125"),
@@ -67,13 +68,15 @@ class ChatGPT(commands.Cog):
         model: str = "gpt-4-turbo",
     ):
         """
-        Generate text in response to a given prompt using the specified GPT model and personality.
+        Creates a model response for the given chat conversation.
 
         Args:
-            ctx: The context in which the command was called.
-            prompt: The prompt for the GPT model.
-            personality: The personality for the GPT model.
-            model: The GPT model to use.
+          messages: A list of messages comprising the conversation so far.
+              [Example Python code](https://cookbook.openai.com/examples/how_to_format_inputs_to_chatgpt_models).
+
+          model: ID of the model to use. See the
+              [model endpoint compatibility](https://platform.openai.com/docs/models/model-endpoint-compatibility)
+              table for details on which models work with the Chat API.
         """
         await ctx.defer()  # Acknowledge the interaction immediately - reply can take some time
         try:
@@ -108,14 +111,19 @@ class ChatGPT(commands.Cog):
     )
     @option("prompt", description="Prompt", required=True)
     @option(
-        "size",
-        description="Size of the image in pixels (default: 1024x1024)",
+        "model",
+        description="Choose from the following DALL-E models (default: dall-e-3)",
         required=False,
         choices=[
-            OptionChoice(name="1024x1024", value="1024x1024"),
-            OptionChoice(name="1024x1792 (landscape)", value="1024x1792"),
-            OptionChoice(name="1792x1024 (portrait)", value="1792x1024"),
+            OptionChoice(name="DALL-E 2", value="dall-e-2"),
+            OptionChoice(name="DALL-E 3", value="dall-e-3"),
         ],
+    )
+    @option(
+        "n",
+        description="Number of images to generate (default: 1)",
+        required=False,
+        type=int,
     )
     @option(
         "quality",
@@ -127,52 +135,110 @@ class ChatGPT(commands.Cog):
         ],
     )
     @option(
-        "n",
-        description="Number of images to generate (default: 1)",
-        required=False,
-        type=int,
-    )
-    @option(
-        "model",
-        description="Choose from the following DALL-E models (default: dall-e-3)",
+        "size",
+        description="Size of the image in pixels. Note that sizes 256x256 and 512x512 are only supported by \
+                DALL-E 2. (default: 1024x1024)",
         required=False,
         choices=[
-            OptionChoice(name="DALL-E 3", value="dall-e-3"),
-            OptionChoice(name="DALL-E 2", value="dall-e-2"),
+            OptionChoice(name="256x256", value="256x256"),
+            OptionChoice(name="512x512", value="512x512"),
+            OptionChoice(name="1024x1024", value="1024x1024"),
+            OptionChoice(name="1024x1792 (landscape)", value="1024x1792"),
+            OptionChoice(name="1792x1024 (portrait)", value="1792x1024"),
+        ],
+    )
+    @option(
+        "style",
+        description="Style of the image. Only supported for DALL-E 3. (default: natural)",
+        required=False,
+        choices=[
+            OptionChoice(name="Vivid", value="vivid"),
+            OptionChoice(name="Natural", value="natural"),
         ],
     )
     async def generate_image(
         self,
         ctx: ApplicationContext,
         prompt: str,
+        model: str = "dall-e-3",
         n: int = 1,
         quality: str = "standard",
-        model: str = "dall-e-3",
         size: str = "1024x1024",
-        style: str = "",
+        style: Optional[str] = "natural",
     ):
         """
-        Generate an image from a given prompt using the specified DALL-E model, size, and quality.
+        Creates an image given a prompt.
 
         Args:
-            ctx: The context in which the command was called.
-            prompt: The prompt for the DALL-E model.
-            n: The number of images to generate.
-            quality: The quality of the generated image.
-            model: The DALL-E model to use.
-            size: The size of the generated image.
-            style: The style of the generated image.
+          prompt: A text description of the desired image(s). The maximum length is 1000
+              characters for `dall-e-2` and 4000 characters for `dall-e-3`.
+
+          model: The model to use for image generation.
+
+          n: The number of images to generate. Must be between 1 and 10. For `dall-e-3`, only
+              `n=1` is supported.
+
+          quality: The quality of the image that will be generated. `hd` creates images with finer
+              details and greater consistency across the image. This param is only supported
+              for `dall-e-3`.
+
+          size: The size of the generated images. Must be one of `256x256`, `512x512`, or
+              `1024x1024` for `dall-e-2`. Must be one of `1024x1024`, `1792x1024`, or
+              `1024x1792` for `dall-e-3` models.
+
+          style: The style of the generated images. Must be one of `vivid` or `natural`. Vivid
+              causes the model to lean towards generating hyper-real and dramatic images.
+              Natural causes the model to produce more natural, less hyper-real looking
+              images. This param is only supported for `dall-e-3`.
         """
         await ctx.defer()  # Acknowledge the interaction immediately - reply can take some time
-        if model == "dall-e-2" and n > 10 or model == "dall-e-3" and n > 1:
+        if (model == "dall-e-2" and n > 10) or (model == "dall-e-3" and n > 1):
+            error_message = (
+                "The maximum number of images for DALL-E 2 is 10 and for DALL-E 3 is 1."
+            )
             await ctx.followup.send(
                 embed=Embed(
-                    title="Error",
-                    description="The maximum number of images for DALL-E 2 is 10 and for DALL-E 3 is 1.",
-                    color=Colour.red(),
+                    title="Error", description=error_message, color=Colour.red()
                 )
             )
             return
+
+        if model == "dall-e-2" and (size == "1024x1792" or size == "1792x1024"):
+            error_message = "The DALL-E 2 model only supports `256x256`, `512x512`, or `1024x1024` image size."
+            await ctx.followup.send(
+                embed=Embed(
+                    title="Error", description=error_message, color=Colour.red()
+                )
+            )
+            return
+
+        if model == "dall-e-3" and (size == "256x256" or size == "512x512"):
+            error_message = "The DALL-E 3 model only supports `1024x1024`, `1792x1024`, or `1024x1792` image size."
+            await ctx.followup.send(
+                embed=Embed(
+                    title="Error", description=error_message, color=Colour.red()
+                )
+            )
+            return
+
+        if model == "dall-e-2" and quality == "hd":
+            error_message = "The `hd` quality is only supported for DALL-E 3."
+            await ctx.followup.send(
+                embed=Embed(
+                    title="Error", description=error_message, color=Colour.red()
+                )
+            )
+            return
+        
+        if model == "dall-e-2" and style:
+            error_message = "The `style` parameter is only supported for DALL-E 3."
+            await ctx.followup.send(
+                embed=Embed(
+                    title="Error", description=error_message, color=Colour.red()
+                )
+            )
+            return
+
         try:
             response = openai.images.generate(
                 prompt=prompt, n=n, quality=quality, model=model, size=size, style=style
@@ -180,11 +246,10 @@ class ChatGPT(commands.Cog):
             image_url = response.data[0].url if response.data else "No image."
             embed = Embed(
                 title="DALL-E Image Generation",
-                description=f"**Your Prompt:**\n{prompt}",
+                description=f"**Prompt:**\n{prompt}",
                 color=Colour.blue(),
             )
-            embed.set_image(url=image_url)  # Setting the image URL in the embed
-            embed.image.url
+            embed.set_image(url=image_url)
             await ctx.followup.send(embed=embed)
         except Exception as e:
             await ctx.followup.send(
@@ -193,13 +258,26 @@ class ChatGPT(commands.Cog):
 
     @slash_command(
         name="text_to_speech",
-        description="Converts text to lifelike spoken audio.",
+        description="Generates audio from the input text.",
         guild_ids=GUILD_IDS,
     )
-    @option("text", description="Text to convert to speech", required=True)
+    @option(
+        "input",
+        description="Text to convert to speech (max length 4096 characters)",
+        required=True,
+    )
+    @option(
+        "model",
+        description="Choose from the following TTS models (default: tts-1)",
+        required=False,
+        choices=[
+            OptionChoice(name="tts-1", value="tts-1"),
+            OptionChoice(name="tts-1-hd", value="tts-1-hd"),
+        ],
+    )
     @option(
         "voice",
-        description="Choose a voice for the speech",
+        description="Choose a voice for the speech (default: alloy)",
         required=False,
         choices=[
             OptionChoice(name="alloy", value="alloy"),
@@ -229,40 +307,41 @@ class ChatGPT(commands.Cog):
         required=False,
         type=float,
     )
-    @option(
-        "model",
-        description="Choose from the following TTS models",
-        required=False,
-        choices=[
-            OptionChoice(name="tts-1", value="tts-1-"),
-            OptionChoice(name="tts-1-hd", value="tts-1-hd"),
-        ],
-    )
     async def text_to_speech(
         self,
         ctx: ApplicationContext,
-        text: str,
+        input: str,
         model: str = "tts-1",
         voice: str = "alloy",
         response_format: str = "mp3",
         speed: float = 1.0,
     ):
         """
-        Convert a given text to speech using the specified TTS model, voice, format, and speed.
+        Generates audio from the input text.
 
         Args:
-            ctx: The context in which the command was called.
-            text: The text to convert to speech.
-            model: The TTS model to use.
-            voice: The voice for the speech.
-            response_format: The format of the generated audio.
-            speed: The speed of the generated audio.
+          input: The text to generate audio for. The maximum length is 4096 characters.
+
+          model:
+              One of the available [TTS models](https://platform.openai.com/docs/models/tts):
+              `tts-1` or `tts-1-hd`
+
+          voice: The voice to use when generating the audio. Supported voices are `alloy`,
+              `echo`, `fable`, `onyx`, `nova`, and `shimmer`. Previews of the voices are
+              available in the
+              [Text to speech guide](https://platform.openai.com/docs/guides/text-to-speech/voice-options).
+
+          response_format: The format to audio in. Supported formats are `mp3`, `opus`, `aac`, `flac`,
+              `wav`, and `pcm`.
+
+          speed: The speed of the generated audio. Select a value from `0.25` to `4.0`. `1.0` is
+              the default.
         """
         await ctx.defer()  # Acknowledge the interaction immediately - reply can take some time
         try:
             # Generate spoken audio from input text
             response = openai.audio.speech.create(
-                input=text,
+                input=input,
                 model=model,
                 voice=voice,
                 response_format=response_format,
@@ -280,7 +359,7 @@ class ChatGPT(commands.Cog):
             # Inform the user that the audio has been created
             embed = Embed(
                 title="Text to Speech Conversion",
-                description=f"**Text:** {text}\n**Voice:** {voice}\n**Audio File:** Attached",
+                description=f"**Text:** {input}\n**Voice:** {voice}",
                 color=Colour.green(),
             )
             await ctx.followup.send(embed=embed, file=File(speech_file_path))
@@ -288,3 +367,6 @@ class ChatGPT(commands.Cog):
             await ctx.followup.send(
                 embed=Embed(title="Error", description=str(e), color=Colour.red())
             )
+        finally:
+            # Delete the audio file after sending
+            speech_file_path.unlink(missing_ok=True)
