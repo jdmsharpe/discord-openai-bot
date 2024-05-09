0a91fbba532ed0e5e7ccd6ed5adc3944d5b28dbc
John Magnus-Sharpe <jdmsharpe@gmail.com>
2024-05-09 09:37:38 -0700

    Toggle Intents.guilds = True

--- src/bot.py
+++ src/bot.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     intents.presences = False
     intents.members = True
     intents.message_content = True
+    intents.guilds = True
     bot = Bot(intents=intents)
     bot.add_cog(ChatGPT(bot=bot))
     bot.run(BOT_TOKEN)
